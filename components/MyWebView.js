94db0f42dfaf2aeb58689ac0e7053f9f0b3bc676
oouuyab <oouuyab@gmail.com>
2022-10-05 11:20:14 +0900

    feat: set webview

---
+++ components/MyWebView.js
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+import {BackHandler} from 'react-native';
+import {WebView} from 'react-native-webview';
+
+const MyWebView = ({handleClose}) => {
+  const BASE_URL = 'http://localhost:3030';
+  const [webview, setWebview] = useState();
+  const [goBackable, setGoBackable] = useState(false);
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        console.log('goBackable', goBackable);
+        if (goBackable) webview.goBack();
+        else handleClose();
+        return true;
+      },
+    );
+    return () => backHandler.remove();
+  }, [goBackable]);
+  useEffect(() => {
+    if (webview && webview.clearCache) webview.clearCache();
+  }, [webview]);
+  return (
+    <WebView
+      pullToRefreshEnabled={true}
+      startInLoadingState={true}
+      allowsBackForwardNavigationGestures={true}
+      source={{uri: BASE_URL}}
+      mixedContentMode={'compatibility'}
+      originWhitelist={['https://*', 'http://*']}
+      overScrollMode={'never'}
+      // ref={ref => setWebview(ref)}
+      injectedJavaScript={`
+        (function() {
+            function wrap(fn) {
+            return function wrapper() {
+                var res = fn.apply(this, arguments);
+                window.ReactNativeWebView.postMessage(window.location.href);
+                return res;
+            }
+            }
+            history.pushState = wrap(history.pushState);
+            history.replaceState = wrap(history.replaceState);
+            window.addEventListener('popstate', function() {
+            window.ReactNativeWebView.postMessage(window.location.href);
+            });
+        })();
+        true;
+        `}
+      onMessage={event => {
+        const url = event.nativeEvent.data;
+        setGoBackable(url !== BASE_URL);
+        console.log('onMessage', event.nativeEvent.data);
+      }}
+    />
+  );
+};
+
+export default MyWebView;
